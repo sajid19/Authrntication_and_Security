298254e6291fd894b8fead8fe44e7b3e49dde6b1
sajid_f57ovgd <sajidhasan2641@gmail.com>
2022-06-23 12:16:06 -0700

    somechange

--- app.js
+++ app.js
@@ -62,8 +62,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
-    
+    callbackURL: "http://gentle-everglades-78009.herokuapp.com/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);
